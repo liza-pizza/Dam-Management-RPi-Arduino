2e5712c87c9a70e831293336d0b011ce41a4fea2
NimishaMC <60665585+Nimisha0987@users.noreply.github.com>
2022-05-18 18:59:11 +0530

    final changes

--- ATMEGA.ino
+++ ATMEGA.ino
@@ -1,26 +1,28 @@
 #include <avr/sleep.h>
 #include <ArduinoJson.h>
 
-#define echoPin 2 // attach pin D2 Arduino to pin Echo of HC-SR04
-#define trigPin 3 //attach pin D3 Arduino to pin Trig of HC-SR04
+//Arduino
+
+#define echoPin 12 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin 11 //attach pin D3 Arduino to pin Trig of HC-SR04
 const int buttonPin = 2; //Button to perform interrupt
 
 // defines variables
-long duration; // variable for the duration of sound wave travel
+long duration; // variable for theduration of sound wave travel
 int distance; // variable for the distance measurement
 
 void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
   pinMode(echoPin, INPUT_PULLUP); // Sets the echoPin as an INPUT
-  digitalWrite(trigPin, HIGH);
-  // pinMode(echoPin, INPUT);
-  Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
-  Serial.println("Ultrasonic Sensor HC-SR04 Test"); // print some text in Serial Monitor
-  Serial.println("with Arduino UNO R3");
+  digitalWrite(trigPin, HIGH); 
+  
+  Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speedS
+ 
 }
 
 void loop() {
-  
+
+   detachInterrupt(0);
   // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(echoPin, HIGH);
   // Calculating the distance
@@ -28,32 +30,32 @@ void loop() {
   // Displays the distance on the Serial Monitor
   
   DynamicJsonDocument doc(1024);
-  doc["sensor"]= 1;
-  doc["water_depth"] = distance;
+  doc["sensor"]= 9;
+  doc["distance"] = distance;
   doc["site"] = 2;
   String jsonStr;
   serializeJson(doc, jsonStr);
-  Serial.println(jsonStr);
-    Going_to_sleep();
   
+  Serial.println(jsonStr);
+  delay(2000);
+  Going_to_sleep();
 }
 
 void Going_to_sleep() {
-  sleep_enable();
-  Serial.println("Sleep\n");
-  attachInterrupt(digitalPinToInterrupt(buttonPin),wakeUp,LOW);
+sleep_enable();
+
+  attachInterrupt(0,wakeUp,FALLING);
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
   digitalWrite(trigPin, LOW);
   sleep_cpu();
   
-  Serial.println("Just woke up!");
+  //Serial.println("Just woke up!");
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 }
 
 void wakeUp() {
-  Serial.println("Interrupt Fired");
   sleep_disable();
-  detachInterrupt(0);
+     
 }
